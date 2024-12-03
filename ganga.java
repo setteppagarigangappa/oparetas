521d28130bb77d8545a873ca3c28ee8164fa4ae8
setteppagarigangappa <23hr1a0228@mtieat.org>
2024-12-03 10:33:49 +0530

    Add files via upload

---
+++ ganga.java
@@ -0,0 +1,14 @@
+public class ganga{ 
+    public static void main(String[]args){
+int a=10;
+int b=30;
+System .out.println(a=b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a!=b);
+System.out.println(a>b);
+System.out.println(a<b);
+    }
+    
+    
+}
