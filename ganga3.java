2c537c275e645f5f6e17bbedeeb646a4904637f4
setteppagarigangappa <23hr1a0228@mtieat.org>
2024-12-04 11:14:23 +0530

    Add files via upload

---
+++ ganga3.java
@@ -0,0 +1,12 @@
+class ganga3{
+    public static void main(String [] args){
+        int a=5;
+        int pos=0;
+        if((a&(1<<pos))!=0)
+        {
+         System .out.println("set bit");
+        }else{
+          System.out.println("not set bit");
+        }
+    }
+}
